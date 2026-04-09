0e11cf077e2cd546e89319e84ae7cf36e68d23de
Francesco Dammacco <francescodammacco@live.it>
2026-04-09 03:41:02 +0200

    Slides: refactor results

--- slides/src/slides/7-FewShotPromptingResults.jsx
+++ slides/src/slides/7-FewShotPromptingResults.jsx
@@ -6,35 +6,15 @@ export function FewShotPromptingResultsSlide() {
       index={1}
       title="Few-Shot Prompting"
       dataset="few-shot-prompting"
-    >
-      <p className="text-xs font-mono">Trustworthiness: 91.6%</p>
-
-      <p className="text-xs font-mono mb-0!">Attivazione Concettuale: </p>
-      <table className="text-xs m-0! font-mono">
-        <tbody>
-          <tr>
-            <td>Customer Order Issue</td>
-            <td>92.8%</td>
-            <td>15.8%</td>
-          </tr>
-          <tr>
-            <td>Customer Information</td>
-            <td>-</td>
-            <td>42.1%</td>
-          </tr>
-          <tr>
-            <td>Damaged Tablet</td>
-            <td>-</td>
-            <td>42.1%</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <p className="text-xl absolute bottom-0">
-        La divisione in due poli primari riflette geometricamente l'alternanza
+      trustworthiness={91.6}
+      conceptualFootprint={{
+        "Customer Order Issue": [92.8, 15.8],
+        "Customer Information": [0, 42.1],
+        "Damaged Tablet": [0, 42.1],
+      }}
+      description="La divisione in due poli primari riflette geometricamente l'alternanza
         tra la sintassi vincolante del formato richiesto e l'inserimento dei
-        dati estratti dal contesto
-      </p>
-    </ResultSlide>
+        dati estratti dal contesto"
+    />
   );
 }
--- slides/src/slides/templates/ResultSlide.jsx
+++ slides/src/slides/templates/ResultSlide.jsx
@@ -1,6 +1,13 @@
 import { Slide } from "@revealjs/react";
 
-export function ResultSlide({ index, title, dataset, children }) {
+export function ResultSlide({
+  index,
+  title,
+  dataset,
+  trustworthiness,
+  conceptualFootprint,
+  description,
+}) {
   return (
     <Slide
       className="h-full text-left"
@@ -14,7 +21,29 @@ export function ResultSlide({ index, title, dataset, children }) {
           <span className="text-base">Risultati ({index}/3)</span> <br />
           {title}
         </h4>
-        {children}
+        <p className="text-xs font-mono">Trustworthiness: {trustworthiness}%</p>
+
+        <p className="text-xs font-mono mb-0!">Attivazione Concettuale: </p>
+        <table className="text-xs m-0! font-mono">
+          <tbody>
+            {Object.entries(conceptualFootprint).map(
+              ([concept, activations]) => (
+                <tr key={concept}>
+                  <td>{concept}</td>
+                  {activations.map((activation, index) => (
+                    <td key={index}>
+                      {activation === 0 ? "-" : `${activation}%`}
+                    </td>
+                  ))}
+                </tr>
+              ),
+            )}
+          </tbody>
+        </table>
+
+        <p className="text-xl text-center absolute bottom-0 px-25 m-0!">
+          {description}
+        </p>
       </div>
     </Slide>
   );
