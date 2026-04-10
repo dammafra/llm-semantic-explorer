38ea98b472f7ea6eaa666e171cc143c6f05c643e
Francesco Dammacco <francescodammacco@live.it>
2026-04-10 02:24:16 +0200

    FIx dependencies

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -1,12 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import restart from 'vite-plugin-restart'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [restart(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     // host: true,
   },
